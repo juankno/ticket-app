fa29d439dee6e33b3f82ba863e61b3a35f71980b
Juankno <juanes0202@gmail.com>
2025-01-05 17:48:29 -0500

    APP feat: add public and private route of API

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::post('/register', [\App\Http\Controllers\Api\V1\AuthController::class, 'register']);
+Route::post('/login', [\App\Http\Controllers\Api\V1\AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [\App\Http\Controllers\Api\V1\AuthController::class, 'user']);
+    Route::post('/logout', [\App\Http\Controllers\Api\V1\AuthController::class, 'logout']);
+});
