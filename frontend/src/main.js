cc5bb92cd73445554ed7ce48113383d4c290104c
Juankno <juanes0202@gmail.com>
2025-01-05 21:27:27 -0500

    APP feat: import axios and use credential in the config

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,9 +2,14 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import axios from 'axios';
 
 import App from './App.vue'
 import router from './router'
+
+axios.defaults.baseURL = 'http://localhost:8000'
+axios.defaults.withCredentials = true
+axios.defaults.withXSRFToken = true
 
 const app = createApp(App)
 
