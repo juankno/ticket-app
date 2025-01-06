bdfd6c6999194d16a9121ee80192154f94d83207
Juankno <juanes0202@gmail.com>
2025-01-05 19:33:06 -0500

    APP feat: return report of tickets

--- backend/app/Http/Controllers/Api/V1/Ticket/TicketReportController.php
+++ backend/app/Http/Controllers/Api/V1/Ticket/TicketReportController.php
@@ -3,15 +3,22 @@
 namespace App\Http\Controllers\Api\V1\Ticket;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class TicketReportController extends Controller
 {
     /**
-     * Handle the incoming request.
+     * Get the ticket report.
+     *
+     * @param Request $request
+     * @return JsonResponse
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request): JsonResponse
     {
-        //
+        return new JsonResponse([
+            'new' => rand(1, 10),
+            'open' => rand(1, 10),
+        ]);
     }
 }
