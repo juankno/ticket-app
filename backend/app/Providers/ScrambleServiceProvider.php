8567eca3d9e76a2f9d1a84b159a09b2142340d13
Juankno <juanes0202@gmail.com>
2025-01-05 20:45:23 -0500

    APP feat: add authenticated method in the docs

--- backend/app/Providers/ScrambleServiceProvider.php
+++ backend/app/Providers/ScrambleServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Dedoc\Scramble\Scramble;
+use Dedoc\Scramble\Support\Generator\OpenApi;
+use Dedoc\Scramble\Support\Generator\SecurityScheme;
 use Illuminate\Routing\Route;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
@@ -22,6 +24,16 @@ class ScrambleServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+
+        Scramble::afterOpenApiGenerated(function (OpenApi $openApi) {
+            $openApi->secure(
+                SecurityScheme::apiKey('header', 'Authorization')
+                    ->setDescription('Your API key')
+                    ->as('apiKey')
+            );
+        });
+
+
         Scramble::routes(function (Route $route) {
             return Str::startsWith($route->uri, 'api/');
         });
