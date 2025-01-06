3e4d58a037bc8c9135f274338490a39902970da0
Juankno <juanes0202@gmail.com>
2025-01-05 22:48:58 -0500

    APP refactor: change card design

--- frontend/src/components/TicketCard.vue
+++ frontend/src/components/TicketCard.vue
@@ -6,18 +6,19 @@ const props = defineProps({
 </script>
 
 <template>
-	<div class="border border-neutral-300 rounded-xl bg-white p-5 flex flex-col md:flex-row justify-between shadow-lg">
-		<div class="w-full">
-			<div>
-				<div class="flex flex-col md:flex-row justify-between items-start md:items-center m-1">
-					<h3 class="text-xl">{{ ticket.name }}</h3>
-					<div :class="[ ticket.status === 'NEW' ? 'bg-green-100' : 'bg-yellow-100' , 'bg-green-100 p-1 rounded-lg']"><small>{{ ticket.status }}</small></div>
-				</div>
-				<hr class="my-1">
-				<p class="text-neutral-800 m-1">
-					{{ ticket.description }}
-				</p>
-			</div>
+	<div class="max-w-sm rounded overflow-hidden shadow-lg">
+		<img class="w-full" src="https://v1.tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains">
+		<div class="px-6 py-4">
+			<div class="font-bold text-xl mb-2">{{ ticket.title }}</div>
+			<p class="text-gray-700 text-base">
+				{{ ticket.description }}
+			</p>
+		</div>
+		<div class="px-6 pt-4 pb-2">
+			<span :class="[ticket.status === 'NEW' ?
+				'bg-green-100' : 'bg-blue-500',
+				'bg-gray-200 inline-block rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2']">
+				#{{ ticket.status }}</span>
 		</div>
 	</div>
 </template>
