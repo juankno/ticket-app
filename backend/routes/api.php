0d64f8b69d6756378891de5d934e21efaa60e92b
Juankno <juanes0202@gmail.com>
2025-01-05 20:48:03 -0500

    APP refactor: change route position

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -11,7 +11,7 @@ Route::group(['prefix' => 'v1'], function () {
         Route::get('/user', [\App\Http\Controllers\Api\V1\AuthController::class, 'user']);
         Route::post('/logout', [\App\Http\Controllers\Api\V1\AuthController::class, 'logout']);
 
-        Route::apiResource('tickets', \App\Http\Controllers\Api\V1\Ticket\TicketsController::class);
         Route::get('tickets/report', \App\Http\Controllers\Api\V1\Ticket\TicketReportController::class);
+        Route::apiResource('tickets', \App\Http\Controllers\Api\V1\Ticket\TicketsController::class);
     });
 });
